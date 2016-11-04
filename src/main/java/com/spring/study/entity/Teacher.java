a18d59553c072811feb47ba76219c897b15dcd35
xiaoyaguang1993 <xiaoyaguang_1993@163.com>
2016-11-04 21:19:24 +0800

    添加功能

--- src/main/java/com/spring/study/entity/Teacher.java
+++ src/main/java/com/spring/study/entity/Teacher.java
@@ -13,6 +13,7 @@ public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;//ID
+    private String work_num;//职工号
     private String name;//姓名
     private String user_name;//用户名
     private String password;//密码
@@ -34,6 +35,14 @@ public class Teacher {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getWork_num() {
+        return work_num;
+    }
+
+    public void setWork_num(String work_num) {
+        this.work_num = work_num;
     }
 
     public String getName() {
