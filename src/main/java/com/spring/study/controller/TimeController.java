b00b34c8db91fd9ffb1855043334448b8e47e123
xiaoyaguang1993 <xiaoyaguang_1993@163.com>
2016-11-06 23:58:33 +0800

    xiao 23:58

--- src/main/java/com/spring/study/controller/TimeController.java
+++ src/main/java/com/spring/study/controller/TimeController.java
@@ -1,7 +1,13 @@
 package com.spring.study.controller;
 
+import com.spring.study.entity.Time;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 /**
  * Created by Administrator on 2016/11/6.
@@ -11,5 +17,11 @@ public class TimeController {
     @GetMapping("/timePage")
     public String timePage(){
         return "timeList";
+    }
+    @PostMapping("/timeSet")
+    public String timeSet(@RequestParam("lesson") String lesson, @RequestParam("start_time")String start_time, @RequestParam("end_time") String end_time, HttpSession session){
+        System.out.println(lesson);
+        System.out.println(" :"+"开始："+start_time+"结束："+end_time);
+        return "index";
     }
 }
--- src/main/webapp/js/my.js
+++ src/main/webapp/js/my.js
@@ -1,60 +1,8 @@
 /*管理员登录前台数据校验*/
-$(document).ready(function () {
-
-    /*前台数据校验*/
-    $("#loginButton").bind("click", function () {
-        if ($("#user_name").val() == '') {
-            alert("账号不能为空！！！");
-            return false;
-        } else if ($("#password").val() == '') {
-            alert("密码不能为空！！！");
-            return false;
-        }
-        //异步提交url
-        var url = "admin_login";
-        //提交表单参数
-        var params = $("#manageForm").serialize();
-        $.post(url,params,function(data) {
-            if(data== 1){
-                location.href = "teacherAddPage";
-            }else if(data == 0){
-                alert("密码错误！！！");
-            }else if(data== -1){
-                alert("用户名不存在！！！");
-            }
-        });
-    });
 
 
-    $("#user").bind("focusout",function () {
-        $("#password").val($("#work_num").val());
-    })
-
-    $("#addButton").bind("click", function () {
-        if($("#work_num").val()=='') {
-            alert("教工号不能为空！！！");
-            return false;
-        }else if($("#name").val()=='') {
-            alert("教师名称不能为空！！！");
-            return false;
-        }else if($("#password").val()==''){
-            alert("密码不能为空！！！");
-            return false;
-        }
-        /*访问路径*/
-        var url = "add";
-        /*访问参数*/
-        var params = $("#addTeacherForm").serialize();
-        $.post(url,params,function(data){
-            if(data==1){
-                alert("12");
-            }else if(data==0) {
-                alert("用户名已存在！！！");
-            }
-        });
-    });
 
 
-});
+
 
 
