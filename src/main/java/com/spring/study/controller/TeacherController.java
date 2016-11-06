452f5843ae1295a95e4897aadb42cddb1eb631f3
xiaoyaguang1993 <xiaoyaguang_1993@163.com>
2016-11-06 14:01:13 +0800

    xiao admin login

--- src/main/java/com/spring/study/controller/TeacherController.java
+++ src/main/java/com/spring/study/controller/TeacherController.java
@@ -4,10 +4,7 @@ import com.spring.study.dao.TeacherDao;
 import com.spring.study.entity.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -45,5 +42,9 @@ public class TeacherController {
             status = "-1";
         }
         return status;
+    }
+    @GetMapping(value = "/teacherAddPage")
+    public String TeacherAddPage(){
+        return "addteacher";
     }
 }
